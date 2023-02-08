06ef2e7a39959ae592e6c8fc959d67bbecdcf9b7
shyamabhi <shyamtale396@gmail.com>
2023-02-08 15:47:31 +0530

    hello

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,22 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        Product p = new Product();
+        p.product(5,1);
+        p.product(5,1,2);
+        p.product(5,4);
 
+    }
+    public static class Product{
+        public int product(int x, int y) {
+            return x;
+        }
+        public int product(int x, int y, int z) {
+            return x;
+        }
+        public double product(double x, double y) {
+            return x;
+        }
+    }
 }
